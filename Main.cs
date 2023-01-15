e9306bc2e38dd77dab8680576610199e2f33231b
Ng Kock Leong <drago888@gmail.com>
2023-01-15 22:21:58 +0800

    Fix indentation for codes

--- Main.cs
+++ Main.cs
@@ -12,95 +12,95 @@ namespace AssaultMage
 {
     public static class Main
     {
-            public static bool Enabled;
-            internal static readonly LogWrapper Logger = LogWrapper.Get("AssaultMage");
+        public static bool Enabled;
+        internal static readonly LogWrapper Logger = LogWrapper.Get("AssaultMage");
 
 
-            public static bool Load(UnityModManager.ModEntry modEntry)
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                modEntry.OnToggle = OnToggle;
+                var harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll();
+
+                Logger.Info("Finished patching.");
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Failed to patch", e);
+            }
+            return true;
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Enabled = value;
+            return true;
+        }
+
+        [HarmonyPatch(typeof(BlueprintsCache))]
+        static class BlueprintsCaches_Patch
+        {
+            private static bool Initialized = false;
+
+            [HarmonyPriority(Priority.First)]
+            [HarmonyPatch(nameof(BlueprintsCache.Init)), HarmonyPostfix]
+            static void Init()
             {
                 try
                 {
-                    modEntry.OnToggle = OnToggle;
-                    var harmony = new Harmony(modEntry.Info.Id);
-                    harmony.PatchAll();
+                    if (Initialized)
+                    {
+                        Logger.Info("Already configured blueprints.");
+                        return;
+                    }
+                    Initialized = true;
 
-                    Logger.Info("Finished patching.");
+                    Logger.Info("Configuring blueprints.");
+
+                    AddChanges();
                 }
                 catch (Exception e)
                 {
-                    Logger.Error("Failed to patch", e);
+                    Logger.Error("Failed to configure blueprints.", e);
                 }
-                return true;
             }
+        }
 
-            public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        [HarmonyPatch(typeof(StartGameLoader))]
+        static class StartGameLoader_Patch
+        {
+            private static bool Initialized = false;
+
+            [HarmonyPatch(nameof(StartGameLoader.LoadPackTOC)), HarmonyPostfix]
+            static void LoadPackTOC()
             {
-                Enabled = value;
-                return true;
-            }
-
-            [HarmonyPatch(typeof(BlueprintsCache))]
-            static class BlueprintsCaches_Patch
-            {
-                private static bool Initialized = false;
-
-                [HarmonyPriority(Priority.First)]
-                [HarmonyPatch(nameof(BlueprintsCache.Init)), HarmonyPostfix]
-                static void Init()
+                try
                 {
-                    try
+                    if (Initialized)
                     {
-                        if (Initialized)
-                        {
-                            Logger.Info("Already configured blueprints.");
-                            return;
-                        }
-                        Initialized = true;
-
-                        Logger.Info("Configuring blueprints.");
-
-                        AddChanges();
+                        Logger.Info("Already configured delayed blueprints.");
+                        return;
                     }
-                    catch (Exception e)
-                    {
-                        Logger.Error("Failed to configure blueprints.", e);
-                    }
+                    Initialized = true;
+
+                    RootConfigurator.ConfigureDelayedBlueprints();
                 }
-            }
-
-            [HarmonyPatch(typeof(StartGameLoader))]
-            static class StartGameLoader_Patch
-            {
-                private static bool Initialized = false;
-
-                [HarmonyPatch(nameof(StartGameLoader.LoadPackTOC)), HarmonyPostfix]
-                static void LoadPackTOC()
+                catch (Exception e)
                 {
-                    try
-                    {
-                        if (Initialized)
-                        {
-                            Logger.Info("Already configured delayed blueprints.");
-                            return;
-                        }
-                        Initialized = true;
-
-                        RootConfigurator.ConfigureDelayedBlueprints();
-                    }
-                    catch (Exception e)
-                    {
-                        Logger.Error("Failed to configure delayed blueprints.", e);
-                    }
+                    Logger.Error("Failed to configure delayed blueprints.", e);
                 }
             }
+        }
 
-            public static void AddChanges()
-            {
+        public static void AddChanges()
+        {
             //MyFeat.Configure();
             ArmorAttune.Configure();
             SuperDodge.Configure();
             AssaultMage.Archetypes.AssaultMage.Configure();
-            }
+        }
     }
 }
 
